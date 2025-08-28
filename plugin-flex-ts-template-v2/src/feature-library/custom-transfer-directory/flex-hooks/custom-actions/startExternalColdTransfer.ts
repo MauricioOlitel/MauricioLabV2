6f7229c2574ca655b1b6c0846dc641a9a43606e2
Welder Canesin <>
2025-08-28 15:04:49 -0300

    End Corteva

--- plugin-flex-ts-template-v2/src/feature-library/custom-transfer-directory/flex-hooks/custom-actions/startExternalColdTransfer.ts
+++ plugin-flex-ts-template-v2/src/feature-library/custom-transfer-directory/flex-hooks/custom-actions/startExternalColdTransfer.ts
@@ -26,8 +26,12 @@ export const registerStartExternalColdTransfer = async () => {
         if (!validationCheck.success) {
           Notifications.showNotification(CustomTransferDirectoryNotification.PhoneNumberFailedValidationCheckRequest);
           return;
-        } else if (validationCheck.success && !validationCheck.lookupResponse?.valid) {
-          const errors = validationCheck.lookupResponse.validationErrors.join(', ');
+        } else if (
+          validationCheck.success &&
+          validationCheck.lookupResponse &&
+          validationCheck.lookupResponse.valid === false
+        ) {
+          const errors = (validationCheck.lookupResponse.validationErrors ?? []).join(', ');
           Notifications.showNotification(
             CustomTransferDirectoryNotification.PhoneNumberFailedValidationCheckWithErrors,
             {
